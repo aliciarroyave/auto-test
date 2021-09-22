6a057befcaa34ac386fb2ad2eee59662ebdfe634
Alicia Arroyave <aliciarroyave1021999@gmail.com>
2021-09-22 12:48:15 -0600

    Create test.py

---
+++ test.py
@@ -0,0 +1,11 @@
+def suma(a, b):
+    return a + b
+
+
+def test_suma():
+    assert suma(2, 3) == 5
+    assert suma('gat', 'ito') == 'gatito'
+
+
+def resta(a, b):
+    return a + b  #intencional sera solucionado luego
